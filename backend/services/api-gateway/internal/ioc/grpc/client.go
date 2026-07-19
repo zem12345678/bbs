e35ef5b22afe33cae8ea2dd5597300e113b24894
zhangenmin <1586346727@qq.com>
2026-07-19 23:00:29 +0800

    fix(gateway): isolate etcd resolver lifecycle

--- backend/services/api-gateway/internal/ioc/grpc/client.go
+++ backend/services/api-gateway/internal/ioc/grpc/client.go
@@ -5,7 +5,6 @@ import (
 	"api-gateway/internal/ioc/trace"
 	"api-gateway/pkg/grpc/middleware/exception"
 	"api-gateway/pkg/logger"
-	"context"
 	"encoding/json"
 	"fmt"
 	"sync"
@@ -17,7 +16,6 @@ import (
 	"google.golang.org/grpc/codes"
 	grpcInsecure "google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/keepalive"
-	"google.golang.org/grpc/resolver"
 
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
@@ -196,10 +194,7 @@ func (c *Client) dial(service string, secure bool, options ...ClientOptional) (*
 	}
 
 	etcdRegister := discovery.NewResolver(o.EtcdAddr, o.logger)
-	resolver.Register(etcdRegister)
-
-	_, cancel := context.WithTimeout(context.Background(), o.Timeout)
-	defer cancel()
+	o.GrpcDialOptions = append(o.GrpcDialOptions, grpc.WithResolvers(etcdRegister))
 
 	addr := fmt.Sprintf("%s:///%s", etcdRegister.Scheme(), service)
 	conn, err := grpc.NewClient(addr, o.GrpcDialOptions...)
